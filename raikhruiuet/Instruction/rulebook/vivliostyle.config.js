03637b8f2c0f91b00deac2362a742e21996b7c64
yasusho <yasusho1020@gmail.com>
2025-11-02 03:25:32 +0900

    光る龍の卵など

---
+++ raikhruiuet/Instruction/rulebook/vivliostyle.config.js
@@ -0,0 +1,16 @@
+// @ts-check
+/** @type {import('@vivliostyle/cli').VivliostyleConfigSchema} */
+const vivliostyleConfig = {
+  title: 'raikhruiuet',
+  author: 'author',
+  language: 'x-praige',
+  theme: 'style.css',
+  size: 'B5',
+  orientation: 'landscape',
+  writingMode: 'vertical-rl',
+  
+  entry: [
+    "rule.md"
+  ]
+};
+module.exports = vivliostyleConfig;
